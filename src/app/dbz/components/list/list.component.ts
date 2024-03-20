43dda4d440c243a36ce5742072fb8f32b35cc2e5
Carlos Osorio <caove127@hotmail.com>
2024-03-20 15:06:24 -0500

    commit de prueba

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    },
+    {
+      name: 'Vegeta',
+      power: 8500
+    }
+  ]
+}
